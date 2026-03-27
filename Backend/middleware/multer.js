c7d73a1c601f1749f53562c3efc219a61963c063
light04sk <skmr39630@gmail.com>
2026-03-27 17:25:16 +0530

    Add authentication middleware and multer upload config

---
+++ Backend/middleware/multer.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, unique + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
