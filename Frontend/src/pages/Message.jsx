055a83213a8e4b5ff1d6a0886435ab0392bca508
light04sk <skmr39630@gmail.com>
2026-04-04 14:34:59 +0530

    feat(message): implement socket connection for real-time messaging

--- Frontend/src/pages/Message.jsx
+++ Frontend/src/pages/Message.jsx
@@ -1,12 +1,7 @@
-import React from "react";
 import MessageLayout from "../components/Message/MessageLayout";
 
 const Message = () => {
-  return (
-    <div className="bg-gray-200 h-screen overflow-hidden p-6">
-      <MessageLayout />
-    </div>
-  );
+  return <MessageLayout />;
 };
 
 export default Message;
