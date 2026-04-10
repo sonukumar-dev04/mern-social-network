fd01acb1cd3135e521f8c206f03278f4c3e7f9e2
light04sk <skmr39630@gmail.com>
2026-04-10 20:07:02 +0530

    fix: cors options reference

--- Backend/index.js
+++ Backend/index.js
@@ -18,16 +18,15 @@ import storyRoutes from "./routes/storyRoute.js";
 
 const app = express();
 
-app.options("*", cors());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
